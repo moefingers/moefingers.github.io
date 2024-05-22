6403a8d9a795beb7e26e203a141eadf28ae8f53c
MoeFingers <mbzuiter@gmail.com>
2024-05-21 19:25:38 -0600

    added backface visibility hiddden on each roller item

--- src/components/TextRoller.jsx
+++ src/components/TextRoller.jsx
@@ -15,9 +15,12 @@ export default function TextRoller() {
 
     useEffect(() => {
         const rollerElements = rollerTexts.map((text, index) => {
+            const rotation = index * (360 / rollerTexts.length)
+            if(rotation >= 90 || rotation <= 270){
+
+            }
             return <div key={index} className="text-roller-item" style={{
-                transform: `rotateX(${index * (360 / rollerTexts.length)}deg) rotateZ(0deg) rotateY(0deg) translate3d(0, 0, 25px)`,
-                // top: `calc(var(--general-size-factor-px) * ${(Math.sin((index-1) * ((2 * Math.PI) / rollerTexts.length))) * 20})`,
+                transform: `rotateX(${rotation}deg) rotateZ(0deg) rotateY(0deg) translate3d(0, 0, 25px)`,
             }}>{text}</div>
         })
         console.log(rollerElements)
@@ -30,6 +33,7 @@ export default function TextRoller() {
     return (
         <span className="text-roller-container">
            {...textRollerItems}
+           
         </span>
     )
 }   
