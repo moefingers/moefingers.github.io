7ccadad309bf084a656f37833b079d93c2f94deb
MoeFingers <mbzuiter@gmail.com>
2024-05-23 00:33:39 -0600

    enhanced carousel logic and implemented compatability with poor/older devices

--- src/components/ImageRoller.jsx
+++ src/components/ImageRoller.jsx
@@ -1,26 +1,47 @@
 import { useEffect, useRef, useState } from "react"
 import '../assets/styles/image-roller.css'
-import { colorCycle } from '../assets/animations'
 
-export default function ImageRoller({rollerImages}) {
+export default function ImageRoller({rollerImages, scrollPosition, projectsScrollElement}) {
 
     const [imageRollerItems, setImageRollerItems] = useState([])
 
     useEffect(() => {
         console.log(rollerImages)
         const rollerElements = rollerImages.map((image, index) => {
-           return <img key={index} className="image-roller-item" src={image} style={{
-                transform: `
-                rotateX(${index * (360 / rollerImages.length)}deg) 
-                rotateZ(${index * (360 / rollerImages.length)}deg) 
-                rotateY(0deg) 
+           return (
+            <div key={index} className="image-roller-item">
+                <img onClick={(event) => {scrollToIndex(index)}} src={image} className="backing" style={{
+                    transform: `
+                    rotateX(${index * (-360 / rollerImages.length)}deg) 
+                    rotateZ(${index * (-360 / rollerImages.length)}deg) 
+                    rotateY(0deg) 
+                    
+                    translate3d(
+                        0, 
+                        0, 
+                        calc(${((rollerImages.length / Math.PI) / 2) * 110} * var(--general-size-factor-px)))`,
+                }}/>
+                <img onClick={(event) => {scrollToIndex(index)}} src={image} style={{
+                    transform: `
+                    rotateX(${index * (-360 / rollerImages.length)}deg) 
+                    rotateZ(${index * (-360 / rollerImages.length)}deg) 
+                    rotateY(0deg) 
+                    
+                    translate3d(
+                        0, 
+                        0, 
+                        calc(${((rollerImages.length / Math.PI) / 2) * 120} * var(--general-size-factor-px)))`,
+                }}/>
                 
-                translate3d(
-                    0, 
-                    0, 
-                    calc(${(rollerImages.length / Math.PI) / 2 * 120} * var(--general-size-factor-px)))`,
-            }}/>
+            </div>
+            
+            )
         })
+
+        function scrollToIndex(index) {
+            const eachHeight = (document.querySelector("section").scrollHeight)
+            projectsScrollElement.scrollTop = eachHeight * index
+        }
 
         setImageRollerItems(rollerElements)
         /* 360   -    7 / 360      to fix off by one */
@@ -28,14 +49,21 @@ export default function ImageRoller({rollerImages}) {
         const fullRotation = `${360 - partialRotation}deg`
         document.documentElement.style.setProperty('--full-rotation', fullRotation)
         document.documentElement.style.setProperty('--partial-rotation', partialRotation + "deg")
-        console.log(fullRotation)
-        console.log(partialRotation)
+        // console.log(fullRotation)
+        // console.log(partialRotation)
 
     }, [])
 
+    const ImageRollerElement = useRef(null)
+    useEffect(() => {
+
+        document.documentElement.style.setProperty('--current-rotation-factor', (scrollPosition / rollerImages.length))
+        // console.log(scrollPosition / rollerImages.length)
+    }, [scrollPosition])
+
 
     return (
-        <div className="image-roller-container">
+        <div ref={ImageRollerElement} className="image-roller-container">
            {...imageRollerItems}
 
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import { projects } from "../assets/projects.json"
 
@@ -7,16 +7,7 @@ import ImageRoller from "../components/ImageRoller"
 import '../assets/styles/projects.css'
 
 const fullImageBlobImport = Object.values(import.meta.glob("@assets/images/*/*.{png,jpg,jpeg,PNG,JPEG,webp,WEBP}", { eager: true, query: '?url', import: 'default' }))
-const images = {}
-projects.forEach((project) => {
-    images[project.imagesFolder] = fullImageBlobImport.map((image) => {
-        if(image.includes(project.imagesFolder)){
-            return image
-        }else{
-            return "https://placekitten.com/g/300/300"
-        }
-    })
-})
+console.log(fullImageBlobImport)
 /* expected:
 {
     "v2-stopwatch": [
@@ -28,36 +19,113 @@ projects.forEach((project) => {
 
 export default function Projects() {
     const [projectSectionElements, setProjectSectionElements] = useState([])
+    const [projectData, setProjectData] = useState({})
+    const [scrollPosition, setScrollPosition] = useState(0)
+    const [images, setImages] = useState({})
+
+    
+
+
+    function fetchAll() {
+        let data = {}
+        projects.forEach(async (project, index) => {
+            // fetch api from project
+            if(project.api){
+                try {
+                    const response = await fetch(project.api,
+                        {
+                            method: 'GET',
+                            headers: {'Content-Type': 'application/vnd.github+json'}
+                        }
+                    )
+                    const newData = await response.json()
+                    // console.log(data)
+                    data = Object.assign({}, data, {[project.snakeName]: await newData})
+                    // console.log(newData)
+                    // console.log(data)
+                    setProjectData(data)
+                    
+                    
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        })
+        // console.log(data)
+       
+    }
+    
+    const projectsScrollElement = useRef(null)
+    useEffect(() => {
+        const imagesPreState = {}
+        projects.forEach((project, index) => {
+            fullImageBlobImport.forEach((image) => {
+                if(image.includes(project.imagesFolder)){
+                    console.log(index, project.imagesFolder, image)
+                    if (!imagesPreState[project.snakeName]) {
+                        imagesPreState[project.snakeName] = []
+                    }
+                    imagesPreState[project.snakeName].push(image)
+                }
+            })
+        })
+        console.log(imagesPreState)
+        setImages(imagesPreState)
+
+        fetchAll()
+
+
+        projectsScrollElement.current.onscroll = () => {
+            const currentPosition = (projectsScrollElement.current.scrollTop)
+            const eachHeight = (document.querySelector("section").scrollHeight)
+
+            // console.log (currentPosition, eachHeight)
+            // console.log(currentPosition / eachHeight)
+            setScrollPosition(currentPosition / eachHeight)
+        }
+
+            // projectsScrollElement.current.scrollTop = 1000 // to scroll
+    }, [])
+
 
     useEffect(() => {
-        const elements = projects.map((project, index) => {
-            if(images[project.imagesFolder][0].length == 0 ){
+        const scrollElement = projectsScrollElement.current
+        const scrollBarWidth = (scrollElement.offsetWidth - scrollElement.clientWidth)
+        scrollElement.style.paddingRight = `${scrollBarWidth / scrollElement.offsetWidth * 103}%`
+    }, [projectSectionElements])
 
+
+ 
+
+    useEffect(() => {
+        const sections = projects.map((project, index) => {
+            if(projectData[project.snakeName]){
+                return (
+                    <section key={index} className="project-section">
+                        <h1 className="project-title">{projectData[project.snakeName].name}</h1>
+                        <p className="project-description">{projectData[project.snakeName].description}</p>
+                        <p>from api</p>
+                        {index == 0 && <div className="scroll-note">scroll down...</div>}
+                    </section>
+                )
+            } else {
+                return (
+                    <section key={index} className="project-section">
+                        <h1 className="project-title">{project.name}</h1>
+                        <p className="project-description">{project.description}</p>
+                    </section>
+                )
             }
-            return (
-                <section className="project-section" key={index}>
-                    <h2>{project.name}</h2>
-
-                    <p>{project.description}</p>
-
-                    <p>{project.coolest}</p>
-
-                    <p>{project.hottest}</p>
-
-                    <p>{project.tags}</p>
-
-                    <img src={images[project.imagesFolder][0]} alt="" />
-                </section>
-            )
         })
-
-        setProjectSectionElements(elements)
-    }, [])
+        setProjectSectionElements(sections)
+    }, [projectData])
 
     return (
         <div className="projects-page-container">
+            <div ref={projectsScrollElement} className='scroll' >
                 {...projectSectionElements}
-            <ImageRoller rollerImages={Object.values(images).map((imageSet) => imageSet[0])}/>
+                <ImageRoller scrollPosition={scrollPosition} projectsScrollElement={projectsScrollElement.current} rollerImages={Object.values(images).map((imageSet) => imageSet[0])}/>
+            </div>
         </div>
     )
 }
