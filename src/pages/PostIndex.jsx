933c9f486dd17eb3a170ccaa20b5752426b8c05a
MoeFingers <mbzuiter@gmail.com>
2024-05-26 01:35:56 -0600

    minor changes on post handling

--- src/pages/PostIndex.jsx
+++ src/pages/PostIndex.jsx
@@ -6,7 +6,7 @@ export default function PostIndex() {
     return (
         <div className="posts-index-page-container">
             <ul>
-                {posts.map((post) => <li key={post.path}><Link to={post.path}>{post.path}</Link></li>)}
+                {posts.filter(({ hide }) => !hide).map((post) => <li key={post.path}><Link to={post.path}>{post.path}</Link></li>)}
             </ul>
         </div>
     )
