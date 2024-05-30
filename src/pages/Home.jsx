e3d2cda7f6e9d167eff606a830628b4c5d3a9a24
MoeFingers <mbzuiter@gmail.com>
2024-05-29 21:16:09 -0600

    hid scrollbar on main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 
 import '../assets/styles/home.css'
 import TextRoller from '../components/TextRoller'
+
+import { useEffect, useRef, useState } from 'react'
 // glob import
 const fullImageBlobImport = Object.values(import.meta.glob("@assets/images/homepage/*.{png,jpg,jpeg,PNG,JPEG,webp,WEBP,svg}", { eager: true, query: '?url', import: 'default' }))
 const fullReds = fullImageBlobImport.filter((blob) => blob.includes("fullreds"))
 console.log(fullReds)
+const standingByScooter = fullImageBlobImport.filter((blob) => blob.includes("standing-by-scooter"))
 
 export default function Home() {
 
@@ -39,19 +42,31 @@ export default function Home() {
         
     }
 
+
+    const scrollElementRef = useRef(null)
+        
+    useEffect(() => {
+        const scrollElement = scrollElementRef.current
+        const scrollBarWidth = (scrollElement.offsetWidth - scrollElement.clientWidth)
+        scrollElement.style.paddingLeft = `${scrollBarWidth / scrollElement.offsetWidth * 52}%`
+        scrollElement.style.paddingRight = `${scrollBarWidth / scrollElement.offsetWidth * 52}%`
+    }, [scrollElementRef])
+
     return (    
         <div className="home-page-container">
-            <div className='under-construction'>🚧this site is under heavy construction🚧</div>
-            <div>Sometimes you need an <em>employee</em>... Other times? An <em>asset</em>.</div>
-            <div className="intro">I'm <em>Mohammad Zuiter</em>, {window.innerWidth < 600 ? <br/> : ''}your future<TextRoller/></div>
-            <div>Even on this very site, you are using my <a href="#/Posts/infinity-response">Infinity Response</a> <em>font-size</em> and positioning, 
-                a <em>GitHub API</em> on my <a href="#/Projects">Projects</a> page, and some
-                extremely clever <em>javascript</em> for <em>maximum compatability</em> across older devices.
-            </div>
+            <div className='scroll-element' ref={scrollElementRef}>
+                <div className='under-construction'>🚧this site is under heavy construction🚧</div>
+                <div>Sometimes you need an <em>employee</em>... Other times? An <em>asset</em>.</div>
+                <div className="intro">I'm <em>Mohammad Zuiter</em>, {window.innerWidth < 600 ? <br/> : ''}your future<TextRoller/></div>
+                <div>Even on this very site, you are using my <a href="#/Posts/infinity-response">Infinity Response</a> <em>font-size</em> and positioning, 
+                    a <em>GitHub API</em> on my <a href="#/Projects">Projects</a> page, and some
+                    extremely clever <em>javascript</em> for <em>maximum compatability</em> across older devices.
+                </div>
 
-            <div className='image-container' onMouseMove={handleMouseMoveImage} onTouchMove={handleMouseMoveImage}>
-                <img className='background' src={fullReds} alt="a blurred image of mohammad zuiter in a red blazer" />
-                <img className='foreground' src={fullReds} alt="an overlaying image of mohammad zuiter in a red blazer that is has visible portions on hover" />
+                <div className='image-container' onMouseMove={handleMouseMoveImage} onTouchMove={handleMouseMoveImage}>
+                    <img className='background' src={standingByScooter} alt="a blurred image of mohammad zuiter in a red blazer" />
+                    <img className='foreground' src={standingByScooter} alt="an overlaying image of mohammad zuiter in a red blazer that is has visible portions on hover" />
+                </div>
             </div>
         </div>
     )
